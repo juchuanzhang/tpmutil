6d539f6465b1178ae2257076943e47eba3879d6e
juchuanzhang <juchuanzhang@zju.edu.cn>
2023-07-17 00:43:49 +0800

    2

--- tss/src/param_enc.py
+++ tss/src/param_enc.py
@@ -2,6 +2,7 @@ from .TpmTypes import TPM_ALG_ID
 from .lhmac import NIST
 import hashlib
 from .Crypt import Crypto
+from .Helpers import intToTpm
 
 def KDFa(hashAlg=hashlib.sha1, key=None, label = None, contextU = bytes(), contextV = bytes(), bits = 128):
     obj = NIST.new()
@@ -11,10 +12,29 @@ def KDFa(hashAlg=hashlib.sha1, key=None, label = None, contextU = bytes(), conte
 
 def authHMAC(alg=TPM_ALG_ID.SHA1, sessionKey=None, authValue=None, pHash=None, nonceNewer=None,
              nonceOlder=None, nonceTPMdec=None, nonceTPMenc=None, sessionAttributes=None):
-    key = bytes.fromhex(sessionKey.hex()+authValue.hex())
-    data = bytes.fromhex(pHash.hex()+nonceNewer.hex()+nonceOlder.hex()+nonceTPMdec.hex()+
-                         nonceTPMenc.hex()+sessionAttributes.hex())
+    key = sessionKey + authValue
+    sessionAttributesBytes = intToTpm(sessionAttributes, 1)
+    data = pHash + nonceNewer + nonceOlder + nonceTPMdec + nonceTPMenc + sessionAttributesBytes
+
     digest = Crypto.hmac(alg, key, data)
     return digest
 
-def pHash()
+def cpHash(alg=TPM_ALG_ID.SHA1, cmdCode, name1=None, name2=None, name3=None, param):
+    cmdCodeBytes = intToTpm(cmdCode, 4)
+    data = cmdCodeBytes
+    if(name1 is not None):
+        data = data + name1
+    if (name2 is not None):
+        data = data + name2
+    if (name3 is not None):
+        data = data + name3
+    data = data + param
+    ret = Crypto.hash(alg, data)
+    return ret
+
+def rpHash(alg=TPM_ALG_ID.SHA1, cmdCode, param):
+    cmdCodeBytes = intToTpm(cmdCode, 4)
+    data = cmdCodeBytes
+    data = data + param
+    ret = Crypto.hash(alg, data)
+    return ret
